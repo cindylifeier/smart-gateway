01dbec666b3b36ec937b998a0322ac352130060c
burcakulug <burcak.ulug@feisystems.com>
2018-05-21 11:18:08 -0400

    Create initial project structure based on Zuul
    ##19878:As a Developer, Implement launch context handling in Authorization Server for standalone SMART on FHIR launch - Continued

---
+++ smart-gateway/src/main/java/gov/samhsa/ocp/smartgateway/SmartGatewayApplication.java
@@ -0,0 +1,12 @@
+package gov.samhsa.ocp.smartgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartGatewayApplication.class, args);
+	}
+}
