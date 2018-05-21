9537ea1bd22c883b7c8320e951164d4e2ff1df91
burcakulug <burcak.ulug@feisystems.com>
2018-05-21 13:33:06 -0400

    Add Eureka, Zuul, config-client, and security configurations
    #19878:As a Developer, Implement launch context handling in Authorization Server for standalone SMART on FHIR launch - Continued

--- smart-gateway/src/main/java/gov/samhsa/ocp/smartgateway/SmartGatewayApplication.java
+++ smart-gateway/src/main/java/gov/samhsa/ocp/smartgateway/SmartGatewayApplication.java
@@ -2,11 +2,15 @@ package gov.samhsa.ocp.smartgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableResourceServer
 public class SmartGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmartGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SmartGatewayApplication.class, args);
+    }
 }
---
+++ smart-gateway/src/main/java/gov/samhsa/ocp/smartgateway/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package gov.samhsa.ocp.smartgateway.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+@Configuration
+public class SecurityConfig {
+
+    private static final String RESOURCE_ID = "smart-gateway";
+
+    @Bean
+    public ResourceServerConfigurer resourceServer(SecurityProperties securityProperties) {
+        return new ResourceServerConfigurerAdapter() {
+            @Override
+            public void configure(ResourceServerSecurityConfigurer resources) {
+                resources.resourceId(RESOURCE_ID);
+            }
+
+            @Override
+            public void configure(HttpSecurity http) throws Exception {
+                if (securityProperties.isRequireSsl()) {
+                    http.requiresChannel().anyRequest().requiresSecure();
+                }
+                http.authorizeRequests()
+                        .antMatchers(HttpMethod.GET, "/authorize").permitAll()
+                        .antMatchers(HttpMethod.GET, "/launcher").permitAll()
+                        .antMatchers(HttpMethod.POST, "/token").permitAll()
+                        .antMatchers(HttpMethod.GET, "/launch").denyAll()
+                        .anyRequest().denyAll();
+            }
+        };
+    }
+}
